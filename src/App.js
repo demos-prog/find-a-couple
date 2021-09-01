17760dae621387e7b4e858b2f1b813d50a69cfce
Дмитрий <demosne@gmail.com>
2021-09-01 15:31:34 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { nanoid } from "nanoid";
+import { useState, useMemo } from "react";
+import "./null_styles.css";
+import "./App.css";
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+let arr = shuffle([1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let [resultArr, setResultArr] = useState([]);
+  let [shuffledArray, setShuffledArray] = useState([]);
+
+  let preveusClickNumber = null;
+  let prevIndex = null;
+
+  function handleClick(number, e, index) {
+    if (prevIndex === index) {
+      preveusClickNumber = null;
+      prevIndex = null;
+      setResultArr([...resultArr]);
+    }
+    if (preveusClickNumber === null) {
+      preveusClickNumber = number;
+      prevIndex = index;
+      e.target.classList.add("targeted");
+    } else {
+      if (preveusClickNumber === number) {
+        setResultArr([...resultArr, number]);
+      } else {
+        setResultArr([...resultArr]);
+      }
+      preveusClickNumber = null;
+      prevIndex = null;
+    }
+  }
+
+  useMemo(() => {
+    setShuffledArray(
+      arr.map((item, index) =>
+        resultArr.includes(item) ? (
+          <div key={nanoid()} className="item colored">
+            {item}
+          </div>
+        ) : (
+          <div
+            key={nanoid()}
+            className="item"
+            onClick={(e) => handleClick(item, e, index)}
+          >
+            {item}
+          </div>
+        )
+      )
+    );
+  }, [resultArr]);
+
+  return <div id="field">{shuffledArray}</div>;
 }
 
 export default App;
